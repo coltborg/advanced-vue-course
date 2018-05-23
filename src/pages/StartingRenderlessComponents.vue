c3ae9618583e4563e77a98f7bfa3d5ddcabc2448
Colt Borg <colt.borg@backstopsolutions.com>
2018-05-23 14:29:34 -0500

    Getting started with renderless UI components lesson

--- src/pages/StartingRenderlessComponents.vue
+++ src/pages/StartingRenderlessComponents.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="min-h-screen bg-grey-darker p-8">
     <div class="max-w-sm mx-auto card mt-8">
-      <label class="form-label mb-2">Tags</label>
+      <label class="form-label mb-2">Renderless Tags</label>
+      <renderless-tag-input>
+        <div
+          slot-scope="{}"
+          class="tag-input">
+          <span
+            class="tag-input-tag">
+            <span>sample tag</span>
+            <button
+              type="button"
+              class="tag-input-remove">
+              &times;
+            </button>
+          </span>
+          <input
+            type="text"
+            class="tag-input-text"
+            placeholder="Add tag...">
+        </div>
+      </renderless-tag-input>
+    </div>
+    <div class="max-w-sm mx-auto card mt-8">
+      <label class="form-label mb-2">Original Tags</label>
       <tag-input v-model="tags"/>
     </div>
   </div>
@@ -9,11 +31,13 @@
 
 <script>
 import TagInput from '../components/TagInput';
+import RenderlessTagInput from '../components/RenderlessTagInput';
 
 export default {
   name: 'StartingRenderlessComponents',
   components: {
     TagInput,
+    RenderlessTagInput,
   },
   data () {
     return {
