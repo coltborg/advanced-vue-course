0c1392c8b1b934c8ccbb088df6862016a0ef3daa
Colt Borg <colt.borg@backstopsolutions.com>
2018-06-01 19:07:37 -0500

    Add a second select component and see if it breaks anything

--- src/pages/SearchSelectComponent.vue
+++ src/pages/SearchSelectComponent.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="p-8 min-h-screen bg-grey-darker">
-    <div class="p-8 mx-auto max-w-sm bg-white rounded shadow-lg">
+    <div
+      style="margin-top: 10rem;"
+      class="p-8 mx-auto max-w-sm bg-white rounded shadow-lg">
       <div class="mb-4">
         <label class="mb-2 form-label">Favorite Thrash Band</label>
         <search-select
-          v-model="selectedBand"
-          :options="bands"
+          v-model="selectedTrashBand"
+          :options="trashBands"
+          :filter-function="applySearchFilter" />
+      </div>
+      <div class="mb-4">
+        <label class="mb-2 form-label">Favorite Country Band</label>
+        <search-select
+          v-model="selectedCountryBand"
+          :options="countryBands"
           :filter-function="applySearchFilter" />
       </div>
       <div class="text-right">
@@ -29,8 +38,9 @@ export default {
   },
   data() {
     return {
-      selectedBand: null,
-      bands: [
+      selectedTrashBand: null,
+      selectedCountryBand: null,
+      trashBands: [
         'Anthrax',
         'Dark Angel',
         'Death Angel',
@@ -44,6 +54,19 @@ export default {
         'Sepultura',
         'Slayer',
         'Testament',
+      ],
+      countryBands: [
+        'The Carter Family',
+        'Bill Monroe and His Blue Grass Boys',
+        'Bob Wills',
+        'Flatt & Scruggs',
+        'Alabama',
+        'The Louvin Brothers',
+        'Charlie Daniels Band',
+        'The Flying Burrito Brothers',
+        'The Stanley Brothers',
+        'Dixie Chicks',
+        'The Judds',
       ],
     };
   },
